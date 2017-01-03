ce31c644761e4f89c8c312f08e02ba5570af4790
lbfile <513206491@qq.com>
2017-01-03 23:16:49 +0800

    First commit

---
+++ Model/src/main/java/com/model/common/Basics.java
@@ -0,0 +1,19 @@
+package com.model.common;
+
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: LBfile
+ * Date: 17-1-3
+ * Time: 下午7:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class Basics implements Serializable {
+    private String id;
+    private String createPersonId;
+    private String createTime;
+    private String modifiedPersonId;
+    private String modifiedTime;
+
+}
---
+++ Model/src/main/java/com/model/system/SYS_User.java
@@ -0,0 +1,11 @@
+package com.model.system;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: LBfile
+ * Date: 17-1-3
+ * Time: 下午7:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class SYS_User {
+}
